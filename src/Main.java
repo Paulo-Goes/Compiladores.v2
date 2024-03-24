a52f6cd4561f643c6cf09dc29aa1f2609867dd33
Paulo-Goes <paulo.2021104820@unicap.br>
2024-03-24 14:16:17 -0300

    O que fazer

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Codigo"));
 
+        String s = reader.readLine();
+
+        /* Pegar a linha
+        *  Ler cada caractere até chegar no final de uma palavra
+        *  Comparar palavra com uma lista de itens aceitáveis
+        *  Adicionar na lista de token ou na tabela de símbolos
+        */
 
     }
 }
