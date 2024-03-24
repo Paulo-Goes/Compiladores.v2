10b818012cc6e5c2a0dbba4337cde9158cf459de
Paulo-Goes <paulo.2021104820@unicap.br>
2024-03-24 14:28:12 -0300

    Operadores e palavras reservadas

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Codigo"));
 
         String s = reader.readLine();
+
+        List<String> palavrasReservadas = Arrays.asList("public", "protected", "private", "class", "static", "void", "boolean", "byte", "short", "int", "long", "float", "double", "char");
+        List<String> operadores = Arrays.asList("+", "-", "/", "*", "=", "<", ">", "!");
 
         /* Pegar a linha
         *  Ler cada caractere até chegar no final de uma palavra
