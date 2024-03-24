f60059ecf62ec5ce6bd84b61ab6c1e842fa0f034
Paulo-Goes <paulo.2021104820@unicap.br>
2024-03-24 14:09:54 -0300

    BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader("Codigo"));
+
+
     }
 }
