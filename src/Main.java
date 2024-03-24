812f4f0a1c705eebdc690ce068777c43eb6fda60
Paulo-Goes <paulo.2021104820@unicap.br>
2024-03-24 15:49:01 -0300

    Adiciona um arquivo para colocar o codigo fonte de exemplo
    Codigo para separar o codigo fonte em palavras adicionado (pode estar incompleto)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,46 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("Codigo"));
+        BufferedReader reader = new BufferedReader(new FileReader("Codigo")); //Lê o arquivo com o codigo fonte
 
-        String s = reader.readLine();
+        List<String> listaString = new ArrayList<>();
 
-        List<String> palavrasReservadas = Arrays.asList("public", "protected", "private", "class", "static", "void", "boolean", "byte", "short", "int", "long", "float", "double", "char");
-        List<String> operadores = Arrays.asList("+", "-", "/", "*", "=", "<", ">", "!");
+        String linha;
 
-        /* Pegar a linha
-        *  Ler cada caractere até chegar no final de uma palavra
-        *  Comparar palavra com uma lista de itens aceitáveis
-        *  Adicionar na lista de token ou na tabela de símbolos
-        */
+        while ((linha = reader.readLine()) != null) {
 
+            StringBuilder palavra = null;
+
+            /*
+             * O codigo vai adicionar palavras a uma lista de String
+             * No momento considera-se que se o char verificado for um espaco em branco, a palavra terminou e será colocada na lista
+             */
+
+            for (int i = 0; i < linha.length(); i++) {
+                if (linha.charAt(i) == ' ') {
+                    if (palavra != null) {
+                        listaString.add(palavra.toString());
+                    }
+                    palavra = null;
+                } else {
+                    if (palavra == null) {
+                        palavra = new StringBuilder();
+                    }
+                    palavra.append(linha.charAt(i));
+                }
+            }
+            //Adiciona a ultima palavra da linha
+            if (palavra != null) {
+                listaString.add(palavra.toString());
+            }
+        }
+        for (String s : listaString) {
+            System.out.println(s);
+        }
     }
 }
