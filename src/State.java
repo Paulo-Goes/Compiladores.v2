c6c8a31ede2b1bddf90498e93986781846b398ca
Celso Netto <132786359+cbmnetto1@users.noreply.github.com>
2024-03-25 18:23:03 -0300

    Add files via upload

---
+++ src/State.java
@@ -0,0 +1,14 @@
+package compiladores;
+
+public enum State {
+        INITIAL, 
+        Q1,
+        Q2,
+        Q3,
+        Q4,
+        Q5,
+        Q6,
+        Q7,
+        Q8,
+        ERRO
+}
