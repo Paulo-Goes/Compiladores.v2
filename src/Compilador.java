1bd7ebc2dc9631f3bd000d5446f902ebec76d0c5
Paulo Sérgio Lôbo da Costa Carvalho de Sá Goés <87881165+Paulo-Goes@users.noreply.github.com>
2024-03-26 08:53:33 -0300

    Codigo atualizado
    Funcional

--- src/Compilador.java
+++ src/Compilador.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-public class compilador {
+
+public class Compilador {
 
     public void identificador(ArrayList<String> lista) throws IOException {
         ArrayList<Token> listaSimbolos = new ArrayList<>();
@@ -16,15 +17,17 @@ public class compilador {
             Token token;
             if (lexema.matches("\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|main|native|new|null|package|private|protected|println|public|return|scanf|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\\b")) {
                 token = new Token(Token.tipoToken.KEYWORD, lexema);
-                listaSimbolos.add(token);
+                listaToken.add(token);
             } else if (lexema.matches("\\d+(\\.\\d+)?")) {
                 token = new Token(Token.tipoToken.NUM, lexema);
                 listaToken.add(token);
             } else if (lexema.matches("[a-zA-Z]+[a-zA-Z0-9_-]*")) {
                 token = new Token(Token.tipoToken.ID, lexema);
+                listaSimbolos.add(token);
                 listaToken.add(token);
             } else if (lexema.matches("\"[^\"]*\"")) {
                 token = new Token(Token.tipoToken.TXT, lexema);
+                listaSimbolos.add(token);
                 listaToken.add(token);
             } else if (lexema.matches(".*[+\\-*/%==!=<><=><&|!^+=\\-*/%<<>>>>>]=?")) {
                 token = new Token(Token.tipoToken.OPERADOR, lexema);
@@ -37,6 +40,7 @@ public class compilador {
                 listaToken.add(token);
             } else {
                 token = new Token(Token.tipoToken.INVALIDO, lexema);
+                throw new IllegalArgumentException(token + " não é um token aceito");
             }
         }
         File desktop = new File(FileSystemView.getFileSystemView().getHomeDirectory().toString());
@@ -46,13 +50,13 @@ public class compilador {
         String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yy hh-mm-ss"));
 
         File tokens = new File(folder, "Tokens " + time + ".txt");
-        File simbolos = new File(folder, "Simbolos " + time +".txt");
+        File simbolos = new File(folder, "Simbolos " + time + ".txt");
 
         BufferedWriter writerToken = new BufferedWriter(new FileWriter(tokens));
         BufferedWriter writerSimbolo = new BufferedWriter(new FileWriter(simbolos));
 
-        writerToken.write("Tokens");
-        writerSimbolo.write("Simbolos");
+        writerToken.write("Tokens\n\n");
+        writerSimbolo.write("Simbolos\n\n");
 
         for (Token token : listaToken) {
             writerToken.write(String.valueOf(token) + "\n");
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static java.lang.Character.*;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Codigo2"));
@@ -20,32 +23,35 @@ public class Main {
             for (int i = 0; i < linha.length(); i++) {
                 if (linha.charAt(i) == '"' && !abreApostrofe) {
                     abreAspas = !abreAspas;
+                    if (!abreAspas) {
+                        palavra.append(linha.charAt(i));
+                        listaString.add(palavra.toString());
+                        palavra = null;
+                    }
                 } else if (linha.charAt(i) == '\'' && !abreAspas) {
                     abreApostrofe = !abreApostrofe;
+                    if (!abreApostrofe) {
+                        listaString.add(palavra.toString());
+                        palavra = null;
+                    }
                 }
 
                 if (abreAspas || abreApostrofe) {
-                    if (linha.charAt(i) == '"' || linha.charAt(i) == '\'') {
-                        if(palavra == null){
-                            listaString.add(Character.toString(linha.charAt(i)));
-                        } else{
-                            palavra.append(linha.charAt(i));
-                        }
-                    } else {
-                        if (palavra == null) {
-                            palavra = new StringBuilder();
-                        }
-                        palavra.append(linha.charAt(i));
+                    if (palavra == null) {
+                        palavra = new StringBuilder();
                     }
+                    palavra.append(linha.charAt(i));
                 } else {
-                    // Se for operador ou espaço em branco
-                    if (!Character.isDigit(linha.charAt(i)) && !Character.isAlphabetic(linha.charAt(i)) && !Character.isWhitespace(linha.charAt(i))) {
-                        if (palavra != null) {
-                            listaString.add(palavra.toString());
-                            palavra = null;
+                    // Se for operador
+                    if (!isDigit(linha.charAt(i)) && !isAlphabetic(linha.charAt(i)) && !isWhitespace(linha.charAt(i))) {
+                        if (linha.charAt(i) != '"') {
+                            if (palavra != null) {
+                                listaString.add(palavra.toString());
+                                palavra = null;
+                            }
+                            listaString.add(Character.toString(linha.charAt(i)));
                         }
-                        listaString.add(Character.toString(linha.charAt(i)));
-                    } else if (!Character.isWhitespace(linha.charAt(i))) {
+                    } else if (!isWhitespace(linha.charAt(i))) {
                         if (palavra == null) {
                             palavra = new StringBuilder();
                         }
