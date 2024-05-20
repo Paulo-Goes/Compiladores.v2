c900058cf880b3807c50d7669c5d279b0592d812
Paulo-Goes <paulo.2021104820@unicap.br>
2024-05-20 16:48:39 -0300

    Transformar Token.java em tipo record

--- src/Token.java
+++ src/Token.java
@@ -1,17 +1,1 @@
-public class Token {
-    private final TipoToken tipo;
-    private final String lexema;
-
-    public Token(TipoToken tipo, String lexema) {
-        this.tipo = tipo;
-        this.lexema = lexema;
-    }
-
-    public String getLexema() {
-        return lexema;
-    }
-
-    public TipoToken getTipo() {
-        return tipo;
-    }
-}
+public record Token(TipoToken tipo, String lexema) {}
